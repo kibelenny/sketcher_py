8b0feb94c05d51bda3fac7855c16f34469fe8e9c
leetrix17 <wlenny20@gmail.com>
2022-04-02 12:03:02 +0300

    Testing branch. Image resize

--- sketcher.py
+++ sketcher.py
@@ -5,22 +5,34 @@ import os
 path = input('Enter Image Path: ')
 img = cv2.imread(path)
 
-#Convert to Gray
-gray_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-inverted_gray_image = 255-gray_image
+#Resize Image.
+sizes = [128,256,360,480,512,720,800]
+for size in sizes:
+    aspect_ratio = img.shape[1]/size
+    new_width = int(img.shape[1]/aspect_ratio)
+    new_height = int(img.shape[0]/aspect_ratio)
+    dims = (new_width, new_height)
+    print(dims)
+    img_resized = cv2.resize(img, dims, interpolation=cv2.INTER_AREA)
 
-#Blurring The Inverted Gray Image
-blurred_image = cv2.GaussianBlur(inverted_gray_image, (19,19),0)
-inverted_blurred_image = 255-blurred_image
+    #Convert to Gray
+    gray_image = cv2.cvtColor(img_resized,cv2.COLOR_BGR2GRAY)
+    inverted_gray_image = 255-gray_image
 
-#Sketching
-sketched = cv2.divide(gray_image, inverted_blurred_image,scale= 256.0)
+    #Blurring The Inverted Gray Image
+    blurred_image = cv2.GaussianBlur(inverted_gray_image, (19,19),0)
+    inverted_blurred_image = 255-blurred_image
 
-#Path for exporting image
-dir = os.path.dirname(path)
-name = os.path.basename(path)
-new_name = name.split('.')[0] + '_sketched' + '.' + name.split('.')[-1]
-new_path = os.path.join(dir, new_name)
+    #Sketching
+    sketched = cv2.divide(gray_image, inverted_blurred_image,scale= 256.0)
 
-#Export Image
-cv2.imwrite(new_path,sketched)
+    #Path for exporting image
+    #dir = os.path.dirname(path)
+    dir = r'C:\users\lenny\desktop\outs'
+    name = os.path.basename(path)
+    new_name = name.split('.')[0] + '_ss' + '.' + name.split('.')[-1]
+    new_path = os.path.join(dir, new_name)
+    
+
+    #Export Image
+    cv2.imwrite(new_path,sketched)
